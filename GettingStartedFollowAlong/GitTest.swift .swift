3645c2e1652bcf0756511cf6aeafba4fa80b558e
Jacinda Eng <jceng@uw.edu>
2019-07-02 10:44:49 -0700

    Added new file

---
+++ GettingStartedFollowAlong/GitTest.swift .swift
@@ -0,0 +1,9 @@
+//
+//  GitTest.swift .swift
+//  GettingStartedFollowAlong
+//
+//  Created by Jacinda Eng on 7/2/19.
+//  Copyright © 2019 Michael Panitz. All rights reserved.
+//
+
+import Foundation
